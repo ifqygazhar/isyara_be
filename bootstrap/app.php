44e936a25e2b08699220365ee866d5a642f45862
ifqygazhar <ifqygazhar@gmail.com>
2025-12-03 22:30:40 +0700

    refactor:Remove redundant middleware alias declaration for 'role' in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,8 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
         //
         $middleware->alias([
             'role' => CheckRole::class,
-        ]);
-        $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
     })
